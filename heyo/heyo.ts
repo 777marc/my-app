bc51c60ddc4327a0c23538873eb04269166fa999
Marc Mendoza <777marc@gmail.com>
2024-10-08 09:57:02 -0400

    misc

--- heyo/heyo.ts
+++ heyo/heyo.ts
@@ -3,11 +3,29 @@ import { api } from "encore.dev/api";
 export const get = api(
   { expose: true, method: "GET", path: "/heyo/:word" },
   async ({ word }: { word: string }): Promise<Response> => {
-    const msg = `Heyo... ${word}!`;
-    return { message: msg };
+    const val = await getNutrition(word);
+    console.log("VAL::", val);
+    return { message: JSON.stringify(val) };
   }
 );
 
 interface Response {
   message: string;
 }
+
+const getNutrition = async (item: string) => {
+  const apiKey = process.env.API_KEY;
+  const query = item;
+  const url = `https://api.calorieninjas.com/v1/nutrition?query=${query}`;
+
+  fetch(url, {
+    headers: {
+      "X-Api-Key": apiKey,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("DATA::", data);
+      return data;
+    });
+};
